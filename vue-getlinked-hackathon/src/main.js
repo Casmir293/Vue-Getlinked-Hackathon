92cbe8651032634700304a17e397c5936078f903
Casmir Onyeka <casmir293@gmail.com>
2023-09-25 16:09:27 +0100

    initial commit

---
+++ vue-getlinked-hackathon/src/main.js
@@ -0,0 +1,14 @@
+import "./assets/scss/main.scss";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
+import "@popperjs/core";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
