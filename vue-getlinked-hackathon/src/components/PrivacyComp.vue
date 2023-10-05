c93606d50aa6ee75165406b9df0de691b2dd55bb
Casmir Onyeka <casmir293@gmail.com>
2023-10-05 21:38:40 +0100

    final privacy commit

--- vue-getlinked-hackathon/src/components/PrivacyComp.vue
+++ vue-getlinked-hackathon/src/components/PrivacyComp.vue
@@ -1,5 +1,87 @@
 <template>
-  <div></div>
+  <section class="privacy" id="privacy">
+    <img
+      class="background-img"
+      src="/public/imgs/purple-1.png"
+      alt="background-img"
+    />
+    <img class="star star-1" src="/public/imgs/p-star.png" alt="star" />
+    <img class="star star-2" src="/public/imgs/p-star.png" alt="star" />
+    <img class="star star-3" src="/public/imgs/p-star.png" alt="star" />
+    <img class="star star-4" src="/public/imgs/big-white-star.png" alt="star" />
+    <img class="star star-5" src="/public/imgs/big-white-star.png" alt="star" />
+    <img class="star star-6" src="/public/imgs/star.png" alt="star" />
+    <img class="star star-7" src="/public/imgs/star.png" alt="star" />
+    <section class="privacy-writeup">
+      <div class="privacy-head">Privacy Policy and <span>Terms</span></div>
+      <div class="privacy-body">
+        <p class="privacy-p1">Last updated on September 12, 2023</p>
+        <p class="privacy-p2">
+          Below are our privacy & policy, which outline a lot of goodies. it’s
+          our aim to always take of our participant
+        </p>
+      </div>
+      <div class="privacy-content">
+        <div class="cont-1">
+          At getlinked tech Hackathon 1.0, we value your privacy and are
+          committed to protecting your personal information. This Privacy Policy
+          outlines how we collect, use, disclose, and safeguard your data when
+          you participate in our tech hackathon event. By participating in our
+          event, you consent to the practices described in this policy.
+        </div>
+        <div class="cont-2">
+          <div class="cont-2-a">Licensing Policy</div>
+          <div class="cont-2-b">Here are terms of our Standard License:</div>
+        </div>
+        <div class="cont-3">
+          <div>
+            <img src="/public/imgs/list terms.svg" alt="list" />
+            <p>
+              The Standard License grants you a non-exclusive right to navigate
+              and register for our event
+            </p>
+          </div>
+          <div>
+            <img src="/public/imgs/list terms.svg" alt="list" />
+            <p>
+              You are licensed to use the item available at any free source
+              sites, for your project developement
+            </p>
+          </div>
+        </div>
+        <div class="register-btn">Read More</div>
+      </div>
+    </section>
+    <section class="privacy-graphics">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="249"
+        height="311"
+        viewBox="0 0 249 311"
+        fill="none"
+      >
+        <path
+          d="M249 141.364C249 219.82 195.88 293.188 124.5 311C53.12 293.188 0 219.82 0 141.364V56.5455L124.5 0L249 56.5455V141.364ZM124.5 282.727C176.375 268.591 221.333 205.543 221.333 144.474V74.9227L124.5 30.8173L27.6667 74.9227V144.474C27.6667 205.543 72.625 268.591 124.5 282.727ZM163.233 141.364V120.159C163.233 100.368 143.867 84.8182 124.5 84.8182C105.133 84.8182 85.7667 100.368 85.7667 120.159V141.364C77.4667 141.364 69.1667 149.845 69.1667 158.327V207.805C69.1667 217.7 77.4667 226.182 85.7667 226.182H161.85C171.533 226.182 179.833 217.7 179.833 209.218V159.741C179.833 149.845 171.533 141.364 163.233 141.364ZM145.25 141.364H103.75V120.159C103.75 108.85 113.433 101.782 124.5 101.782C135.567 101.782 145.25 108.85 145.25 120.159V141.364Z"
+          fill="url(#paint0_linear_114_900)"
+          fill-opacity="0.14"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_114_900"
+            x1="124.5"
+            y1="0"
+            x2="124.5"
+            y2="311"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#903AFF" />
+            <stop offset="1" stop-color="#FF26B9" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <div><img src="/public/imgs/privacy.png" alt="privacy" /></div>
+    </section>
+  </section>
 </template>
 
 <script setup></script>
